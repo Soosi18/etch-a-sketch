1518561aefaa1d79a1db1ddbe0963b7f24bc8ade
Soosi18 <sarvagya.pandey2000@yahoo.com>
2024-03-08 20:27:38 +0530

    enableDrawing() now supports custom color input.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 const getWidth = function(){
-
+    let width = prompt("Enter new width of sketch-grid (between 1-100):");
+    return width;
 }
 
 const getHeight = function(){
-
+    let height = prompt("Enter new height of sketch-grid (between 1-100):");
+    return height;
 }
 
-const enableDrawing = function(){
+const enableDrawing = function(color = "black"){
     const squareDivs = document.querySelectorAll(".square");
     squareDivs.forEach(square => square.addEventListener('mouseover', e => {
-        square.style.backgroundColor = 'black';
+        square.style.backgroundColor = color;
     }));
 }
 
 const makeGrid = function(width=32, height=32){
     const container = document.querySelector(".container");
+    //clear old grid
+    container.innerHTML = "";
+
     let size = width * height;
     let boxWidth = container.clientWidth / width;
     let boxHeight = container.clientHeight / height;
@@ -24,12 +29,22 @@ const makeGrid = function(width=32, height=32){
         squareDiv.classList.toggle('square');
         squareDiv.setAttribute('style', `width: ${boxWidth}px; height: ${boxHeight}px;`);
         container.appendChild(squareDiv);
-    }
+    };
 
     enableDrawing();
 }
 
-window.addEventListener('load', function() {
+const enableCustomization = function(){
+    const sizeBtn = document.querySelector('.size-btn');
+    sizeBtn.addEventListener('click', () => {
+        let width = getWidth();
+        let height = getHeight();
+        makeGrid(width, height);
+    });
+}
+
+window.addEventListener('load', () => {
     makeGrid();
+    enableCustomization();
 })
 
